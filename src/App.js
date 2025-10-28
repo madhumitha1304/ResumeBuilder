d3cf55888ff808a6d85f325a84cebfc2415342f7
Madhumitha Kalidoss <madhumithakalidoss021@gmail.com>
2025-10-28 23:17:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
 import About from './Pages/About/About';
-
 function App() {
   return (
     <ResumeState>
